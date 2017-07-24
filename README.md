bc445f7938518ced0e60326d32bc8dcc37b15470
Caleb L <caleb.lenoir@gmail.com>
2017-07-23 22:24:46 -0400

    rework files and get chart showing with data from postgres

--- README.md
+++ README.md
@@ -1,25 +1,2 @@
 # liquid-charts-js
-Liquid Charts is a javascript library for displaying charts built in Liquid on third party sites.
-
-Liquid Charts currently only supports displaying public charts. Capabilities for displaying charts from private datasets will come in later versions.
-
-## Dependencies
-* jQuery 2.2 (http://code.jquery.com/jquery-2.2.0.min.js)
-* momentjs (http://momentjs.com/downloads/moment.js)
-* underscorejs (http://underscorejs.org/underscore-min.js)
-* highcharts (https://code.highcharts.com/highcharts.js)
-
-  For exporting charts, you can also include the highcharts export module (https://code.highcharts.com/modules/exporting.js)
-
-## Usage
-  See example directory for usage. Use the node package http-server (https://github.com/indexzero/http-server) from the example folder to test locally.
-
-## Setup
-1. Include liquid-charting.js or liquid-charts.min.js with dependencies.
-1. Initialize library with server url and ssl flag
-
-  ``Liquid.init("getliquid.io",true)``
-
-1. Call initChart on div where you want the chart to be displayed and pass the ID of the chart.
-
-  ``Liquid.initChart($('#chart'),"5huH969SKL3KcJsKN")``
+This is a custom solution built for Civic Garden Center of Cincinnati.
---
+++ src/index.js
@@ -0,0 +1,232 @@
+import _ from 'lodash';
+import http from 'http';
+import Highcharts from "highcharts/highstock";
+
+const defaultChartOptions = {
+  "title" : {
+    "text" : "CGC Live Permeable Pavement Experiment"
+  },
+  legend: {
+    enabled: true,
+    align: 'left',
+    backgroundColor: '#FFF',
+    borderColor: 'black',
+    borderWidth: 1,
+    layout: 'vertical',
+    verticalAlign: 'top',
+    y: 100,
+    shadow: true
+  },
+  rangeSelector: {
+    selected: 1
+  },
+  "tooltip" : {
+    "shared" : true,
+    "crosshairs" : true,
+    "dateTimeLabelFormats" : {
+      "millisecond" : "%l:%M:%S.%L %P",
+      "second" : "%l:%M:%S %P",
+      "minute" : "%l:%M %P",
+      "hour" : "%l:%M %P",
+      "day" : "%e. %b",
+      "week" : "%e. %b",
+      "month" : "%b '%y",
+      "year" : "%Y"
+    }
+  },
+  "xAxis" : {
+    "type" : "datetime",
+    "dateTimeLabelFormats" : {
+      "millisecond" : "%l:%M:%S.%L %P",
+      "second" : "%l:%M:%S %P",
+      "minute" : "%l:%M %P",
+      "hour" : "%l:%M %P",
+      "day" : "%e. %b",
+      "week" : "%e. %b",
+      "month" : "%b '%y",
+      "year" : "%Y"
+    }
+  },
+  "yAxis" : [
+    {
+      "labels" : {
+        "format" : "{value} in.",
+        "style" : {
+          "color" : "#8bbc21"
+        }
+      },
+      "title" : {
+        "text" : "Rainfall",
+        "style" : {
+          "color" : "#8bbc21"
+        }
+      },
+      "min" : 0,
+      "opposite": true
+    },
+    {
+      "gridLineWidth" : 0,
+      "title" : {
+        "text" : "Volume",
+        "style" : {
+          "color" : "#be5c47"
+        }
+      },
+      "labels" : {
+        "format" : "{value} gal.",
+        "style" : {
+          "color" : "#be5c47"
+        }
+      },
+      "min" : 0,
+      "tooltip" : {
+        "valueSuffix" : " gal."
+      },
+      "opposite" : false
+    }
+  ]
+};
+const rainGuage = {
+  "id" : "D50000000F681B1D",
+  "name" : "Rain Guage 1",
+  "color" : "#be476e",
+  "lineWidth" : 3,
+  "tooltip" : {
+    "valueSuffix" : " in."
+  },
+  "marker" : {
+    "enabled" : false
+  },
+  "data" : []
+};
+const zone1 = {
+  "id" : "BC0000000F968F1D",
+  "name" : "Permeable Asphalt (Zone 1)",
+  "color" : "#476ebe",
+  "type" : "spline",
+  "lineWidth" : 3,
+  "yAxis" : 1,
+  "tooltip" : {
+    "valueSuffix" : " gal."
+  },
+  "marker" : {
+    "enabled" : false
+  },
+  "data" : []
+};
+const zone2 = {
+  "id" : "280000000F861C1D-V",
+  "name" : "Permeable Concrete (Zone 2)",
+  "color" : "#47a9be",
+  "type" : "spline",
+  "lineWidth" : 3,
+  "yAxis" : 1,
+  "tooltip" : {
+    "valueSuffix" : " gal."
+  },
+  "marker" : {
+    "enabled" : false
+  },
+  "data" : []
+};
+const zone3 = {
+  "id" : "520000000FDDDC1D",
+  "name" : "Concrete (Zone 3)",
+  "color" : "#be9847",
+  "type" : "spline",
+  "lineWidth" : 3,
+  "yAxis" : 1,
+  "tooltip" : {
+    "valueSuffix" : " gal."
+  },
+  "marker" : {
+    "enabled" : false
+  },
+  "data" : []
+};
+const zone4 = {
+  "id" : "E10000000DD5271D",
+  "name" : "Paver 1 (Zone 4)",
+  "color" : "#47be98",
+  "type" : "spline",
+  "lineWidth" : 3,
+  "yAxis" : 1,
+  "tooltip" : {
+    "valueSuffix" : " gal."
+  },
+  "marker" : {
+    "enabled" : false
+  },
+  "data" : []
+};
+const zone5 = {
+  "id" : "070000000F87D01D",
+  "name" : "Paver 2 (Zone 5)",
+  "color" : "#be5c47",
+  "type" : "spline",
+  "lineWidth" : 3,
+  "yAxis" : 1,
+  "tooltip" : {
+    "valueSuffix" : " gal."
+  },
+  "marker" : {
+    "enabled" : false
+  },
+  "data" : []
+};
+
+const zones = [rainGuage, zone1, zone2, zone3, zone4, zone5];
+
+
+class CGCLiveChart {
+  constructor(options, elementId) {
+    this.options = _.extend(defaultChartOptions, options);
+
+    this.chart = Highcharts.stockChart(elementId, this.options);
+    this.series = [];
+
+    //get all data
+    this.getAllData();
+
+    //TODO: add data to chart
+
+    //TODO: setup data polling
+
+    //TODO: save data locally??
+
+    return this;
+  }
+
+  getAllData() {
+    const url = 'http://localhost:4000/services/2dc01e504a258402e25137fd167ef11af37e2be79f6e0d9d/get_data';
+
+    let result = "";
+    const req = http.get(url, (res) => {
+      console.log(`STATUS: ${res.statusCode}`);
+      console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        result += chunk;
+      });
+      res.on('end', () => {
+        const datas = JSON.parse(result);
+
+        for(let i=0; i< zones.length; i++){
+          this.series[i] = this.addDataToZone(datas,zones[i]);
+
+          this.chart.addSeries(this.series[i])
+        }
+      });
+    });
+  }
+
+  addDataToZone(datas,zone){
+    let data = _.find(datas,function(d){ return d.id === zone.id });
+    let series = _.clone(zone);
+    series.data = [...zone.data, ...data.data];
+    return series;
+  }
+
+}
+
+window.CGCLiveChart = new CGCLiveChart({}, 'chart');
